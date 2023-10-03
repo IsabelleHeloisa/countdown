c94daed048fff18844fcecb000d321089a89e3c0
IsabelleHeloisa <isabelle.hello123@outlook.com>
2023-10-03 16:49:57 -0300

    feat: create core components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import './App.css'
-
+import Title from './components/Title'
+import Counter from './components/Counter'
+import NewYear from './assets/newyear.jpg'
 function App() {
   return (
-    <div className="App">
-      <h1>Countdow</h1>
+    <div className="App" style={{ backgroundImage: `url(${NewYear})` }}>
+      <div className="container">
+        <Title title="contagem regressiva para 2024" />
+        <div className="coutdown-container">
+          <Counter title="Dias" number={2} />
+          <Counter title="Horas" number={12} />
+          <Counter title="minutos" number={31} />
+          <Counter title="segundos" number={31} />
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/Counter.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Counter({ title, number }) {
+  return (
+    <div className="counter">
+      <p className="counter-number">{number}</p>
+      <h3 className="counter-text">{title}</h3>
+    </div>
+  )
+}
