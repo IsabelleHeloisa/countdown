f176b155061fc5d96d408eb21fa5b8ec6638af08
IsabelleHeloisa <isabelle.hello123@outlook.com>
2023-10-05 17:59:00 -0300

    feat: create home form

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,36 @@
 import React from 'react'
-
+import './Home.css'
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <div className="home">
+      <h2>Monte a sua contagem regressiva</h2>
+      <form className="countdown-form">
+        <label>
+          <span>Título:</span>
+          <input
+            type="text"
+            name="title"
+            placeholder="digite o título do evento"
+          />
+        </label>
+        <label>
+          <span>Data do evento:</span>
+          <input type="date" name="date" />
+        </label>
+        <label>
+          <span>Imagem:</span>
+          <input
+            type="text"
+            name="image"
+            placeholder="Insira a URL da imagem"
+          />
+        </label>
+        <label>
+          <span>Cor do tema:</span>
+          <input type="color" name="color" />
+        </label>
+        <input type="submit" value="Enviar" />
+      </form>
+    </div>
+  )
 }
