0b64d598925567b68639f92281763302a92c4805
IsabelleHeloisa <isabelle.hello123@outlook.com>
2023-10-06 17:49:12 -0300

    feat: gathering form data

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,21 +1,47 @@
 import React from 'react'
 import './Home.css'
+import { useState } from 'react'
 export default function Home() {
+  const [title, setTitle] = useState()
+  const [date, setDate] = useState()
+  const [image, setImage] = useState()
+  const [color, setColor] = useState()
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    const eventObject = {
+      title,
+      date,
+      image,
+      color
+    }
+
+    console.log(eventObject)
+  }
+
   return (
     <div className="home">
       <h2>Monte a sua contagem regressiva</h2>
-      <form className="countdown-form">
+      <form className="countdown-form" onSubmit={handleSubmit}>
         <label>
           <span>Título:</span>
           <input
             type="text"
             name="title"
             placeholder="digite o título do evento"
+            onChange={e => setTitle(e.target.value)}
+            required
           />
         </label>
         <label>
           <span>Data do evento:</span>
-          <input type="date" name="date" />
+          <input
+            type="date"
+            name="date"
+            onChange={e => setDate(e.target.value)}
+            required
+          />
         </label>
         <label>
           <span>Imagem:</span>
@@ -23,11 +49,17 @@ export default function Home() {
             type="text"
             name="image"
             placeholder="Insira a URL da imagem"
+            onChange={e => setImage(e.target.value)}
           />
         </label>
         <label>
           <span>Cor do tema:</span>
-          <input type="color" name="color" />
+          <input
+            type="color"
+            name="color"
+            onChange={e => setColor(e.target.value)}
+            required
+          />
         </label>
         <input type="submit" value="Enviar" />
       </form>
